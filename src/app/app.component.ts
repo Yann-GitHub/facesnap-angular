861864fcdcdb13e95519d1d10a683d275a3524ef
Yann <ybarlet@gmail.com>
2024-11-24 16:43:34 +0100

    Test and fix memory leaks related to observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,18 +16,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     // variable for observable use the $ suffix to indicate that it is an observable !!
-    this.interval$ = interval(1000).pipe(
-      filter((value) => value % 3 === 0),
-      map((value) =>
-        value % 2 === 0
-          ? `Je suis ${value} et je suis pair`
-          : `Je suis ${value} et je suis impair`
-      ),
-      tap((text) => this.logger(text))
-    );
+    // this.interval$ = interval(1000).pipe(
+    //   filter((value) => value % 3 === 0),
+    //   map((value) =>
+    //     value % 2 === 0
+    //       ? `Je suis ${value} et je suis pair`
+    //       : `Je suis ${value} et je suis impair`
+    //   ),
+    //   tap((text) => this.logger(text))
+    // );
   }
 
-  logger(text: string): void {
-    console.log(text);
-  }
+  // logger(text: string): void {
+  //   console.log(text);
+  // }
 }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { interval, Subject, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,13 +11,44 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss',
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   // Le constructeur injecte le service FaceSnapsService
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getFaceSnaps();
+    this.destroy$ = new Subject<boolean>();
+
+    // test Observable memory leak - create multiple instances of the observable
+    // interval(1000)
+    //   .pipe(tap((value) => console.log(value)))
+    //   .subscribe();
+
+    // test Observable memory leak - create a single instance of the observable
+    //   interval(1000)
+    //     .pipe(
+    //       take(1),
+    //       tap((value) => console.log(value))
+    //     )
+    //     .subscribe();
+    // }
+
+    // test Observable memory leak - using takeUntil operator to unsubscribe from the observable
+    interval(1000)
+      .pipe(
+        takeUntil(this.destroy$),
+        tap((value) => console.log(value))
+      )
+      .subscribe();
+  }
+
+  // ngOnDestroy method is called when the component is destroyed - Lifecycle hook
+  ngOnDestroy(): void {
+    // unsubscribe from the observable
+    this.destroy$.next(true);
+    console.log('FaceSnapListComponent destroyed');
   }
 }
